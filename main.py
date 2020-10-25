ccb01da9750f713317e31377e758ada4a0d25cae
Eduard Gómez <eduard.gomez.escandell@gmail.com>
2020-10-26 00:14:41 +0100

    Made example file more comprehensive

--- main.py
+++ main.py
@@ -2,19 +2,37 @@ import solvers
 import errors
 from sudoku import Sudoku
 
+# Example program
+# This example program solve the same sudoku with two diffferent solvers
+#
+
+filename = 'data/3'
+
 s = Sudoku()
-s.load('data/3')
+s.load(filename)
 print('       GIVENS')
 print(s)
 s.AssertCorrect()
 
-print('Trying the logic algorithm')
-solver = solvers.SinglesSolver(s,maxiter=1000,debug_lvl=1)
+# Trying to solve it with a logical algorithm
+print('Trying the logic algorithm\n')
+solver = solvers.SinglesSolver(s,maxiter=1000,debug_lvl=2)
 try:
     solver.Execute()
-except errors.CannotProgressError as e:
+    solver.Verify()
+except Exception as e:
+    print('')
     print(e)
-    print('Trying the branching algorithm')
-    solver = solvers.BranchingSolver(s,maxiter=1000,debug_lvl=1)
+    
+# # Trying to solve with a branching algorithm
+s = Sudoku()
+s.load(filename)
+print('-'*80)
+print('Trying the branching algorithm\n')
+solver = solvers.BranchingSolver(s,maxiter=1000,debug_lvl=2,Info={'SinglesSolver':{'maxiter':1000,'debug_lvl':1}})
+try:
     solver.Execute()
-solver.Verify()
+    solver.Verify()
+except Exception as e:
+    print('')
+    print(e)
