a99297d01dd62e3c1da9ed4c46284664a8dfc678
Eduard Gómez <eduard.gomez.escandell@gmail.com>
2020-10-25 21:29:46 +0100

    Made method calls more flexible

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ print(s)
 s.AssertCorrect()
 
 print('Trying the logic algorithm')
-solver = solvers.SinglesSolver(s,1000,1)
+solver = solvers.SinglesSolver(s,maxiter=1000,debug_lvl=1)
 try:
     solver.Execute()
 except errors.CannotProgressError as e:
     print(e)
     print('Trying the branching algorithm')
-    solver = solvers.BranchingSolver(s,1000,1)
+    solver = solvers.BranchingSolver(s,maxiter=1000,debug_lvl=1)
     solver.Execute()
 solver.Verify()
--- solvers.py
+++ solvers.py
@@ -1,20 +1,30 @@
 from errors import SolveError, CellLevelError, VerificationError, CannotProgressError, ReachedMaxIterError
 
 class Solver:
-    def __init__(self, sudoku, maxiter=1000, debug_lvl=0):
+    def __init__(self, sudoku, *args, **kwargs):
+        self.maxiter=1000
+        self.debug_lvl=0
+        if 'maxiter'   in kwargs: self.maxiter  = kwargs['maxiter']
+        if 'debug_lvl' in kwargs: self.debug_lvl= kwargs['debug_lvl']
+        
         self.sudoku = sudoku
-        self.maxiter = maxiter
-        self.debug_lvl = debug_lvl
         self.child_solvers = []
-        self.extra_init()
+        self.extra_init(args, kwargs)
         # DEBUG_LVL:
         # 0: No prints
         # 1: Only final result
         # 2: Step-by-step info
         # 3: Step-by-step info and partial results
-    def extra_init(self):
+    
+    def extra_init(self,*args, **kwargs):
         pass
     
+    def print(self, *msg, **kwargs):
+        debug_lvl_required = 0
+        if 'debug_lvl'  in kwargs: debug_lvl_required = kwargs['debug_lvl']
+        if self.debug_lvl > debug_lvl_required:
+            print(*msg)
+        
     def SwitchSudoku(self, new_sudoku):
         self.sudoku = new_sudoku
         for c in self.child_solvers:
@@ -25,6 +35,7 @@ class Solver:
             for npasses in range(1,1+self.maxiter):
                 finished = self.iterate_once(npasses)
                 if finished: break
+                self.print(self.sudoku,debug_lvl=2)
         except KeyboardInterrupt:
             pass
         self.Finalize(npasses)
@@ -48,11 +59,8 @@ class Solver:
         if npasses==self.maxiter:
             raise ReachedMaxIterError()
         
-        if self.debug_lvl > 1 : print('')
-        if self.debug_lvl > 0:
-            print('\n       SOLUTION')
-            print(self.sudoku)
-            print('%d passes employed'%npasses)
+        self.print(' ',debug_lvl=1)
+        self.print('\n       SOLUTION\n%s%d passes employed'%(self.sudoku,npasses), debug_lvl=0)
         
         if self.sudoku.UpdateUncertainty() > 0:
             raise SolveError('Failed to remove all uncertainty')
@@ -60,12 +68,12 @@ class Solver:
     def Verify(self):
         try:
             self.sudoku.AssertCorrect()
-            if self.debug_lvl > 0 : print('The grid is correct')
+            self.print('The grid is correct')
         except VerificationError:
-            if self.debug_lvl > 0 : print('The grid is INCORRECT')
+            self.print('The grid is INCORRECT')
 
 class SinglesSolver(Solver):
-    def extra_init(self):
+    def extra_init(self, *args, **kwargs):
         self.sudoku.UpdateUncertainty()
         
     def iterate_once(self,i):
@@ -83,45 +91,49 @@ class SinglesSolver(Solver):
         return False
 
 class BranchingSolver(Solver):
-    def extra_init(self):
-        if self.debug_lvl > 1 : print(' START - %.2f%% uncertainty\n'%(100*self.sudoku.UpdateUncertainty()))
+    # Necessary methods
+    def extra_init(self, *args, **kwargs):
+        self.print(' START - %.2f%% uncertainty\n'%(100*self.sudoku.UpdateUncertainty()), debug_lvl=1)
         self.snapshots = []
         self.child_solvers.append(SinglesSolver(self.sudoku, 5))
     
     def iterate_once(self,i):
-        cannot_progress = False
         no_uncertainty = False
         try:
             for child in self.child_solvers:
                 no_uncertainty = child.iterate_once(i)            
                 if no_uncertainty:
                     self.sudoku.AssertNoDuplicates()
-                    if self.debug_lvl > 1 : print('<'*len(self.snapshots),'Step %d - Solved (0%% uncertainty)'%i)
+                    self.print('<'*len(self.snapshots),'Step %d - Solved (0%% uncertainty)'%i, debug_lvl=1)
                     return True
-                if self.debug_lvl > 1 : print('|'*len(self.snapshots) + ' Step %d - Reduced uncertainty to %.2f%%'%(i, 100*self.sudoku.UpdateUncertainty()))
+                self.print('|'*len(self.snapshots) + ' Step %d - Reduced uncertainty to %.2f%%'%(i, 100*self.sudoku.UpdateUncertainty()),debug_lvl=1)
             
         except CellLevelError as e:
-            try:
-                self.SwitchSudoku(self.snapshots.pop())
-            except IndexError:
-                raise SolveError('Unexpected failed branch popping')
-            self.sudoku.UpdateUncertainty()
-            if self.debug_lvl > 1 : print('|'*len(self.snapshots) +'< Step %d'%i,'- Impossibility reached at %s'%e.GetCellCoords())
-            if self.debug_lvl > 2 : print(self.sudoku)
+            self.revert_branch()
+            self.print('|'*len(self.snapshots) +'< Step %d'%i,'- Impossibility reached at %s'%e.GetCellCoords(),debug_lvl=1)
             return False
         
         except CannotProgressError:
             index = self.cell_fewest_candidates()
             guess = self.sudoku[index].candidates[0]
-            if self.debug_lvl > 1 : print('|'*(len(self.snapshots)) 
-                + '> Step %i - Guessing %s is %d out of'%(i,self.sudoku[index].GetCoords(),guess),self.sudoku[index].candidates)
-            self.snapshots.append(self.sudoku.copy())
-            self.snapshots[-1][index].RemoveOption(guess)
-            self.sudoku[index].Resolve(guess)
-            self.sudoku.UpdateUncertainty()            
-
-        if self.debug_lvl > 2 : print(self.sudoku)
+            self.print('|'*(len(self.snapshots)) + '> Step %i - Guessing %s is %d out of'%(i,self.sudoku[index].GetCoords(),guess),self.sudoku[index].candidates,debug_lvl=1)
+            self.take_branch(index, guess)
         return False
+    
+    # Support methods
+    
+    def take_branch(self, index, guess):
+        self.snapshots.append(self.sudoku.copy())
+        self.snapshots[-1][index].RemoveOption(guess)
+        self.sudoku[index].Resolve(guess)
+        self.sudoku.UpdateUncertainty()
+        
+    def revert_branch(self):
+        try:
+            self.SwitchSudoku(self.snapshots.pop())
+        except IndexError:
+            raise SolveError('Unexpected failed branch popping')
+        self.sudoku.UpdateUncertainty()
                 
     def cell_fewest_candidates(self):
         min_n_candidates = 10
