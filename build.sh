1bb31398a86d13ee2cb5c91cbb68edda266a09aa
Eduard Gómez <eduard.gomez.escandell@gmail.com>
2020-11-08 16:30:38 +0100

    Small change to compilation

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 SUDOKU_DIR=$PWD
 BUILD_DIR=$SUDOKU_DIR/build
 SOURCE_DIR=$SUDOKU_DIR/source
+SCRIPTS_DIR=$SUDOKU_DIR/scripts
 
 mkdir $BUILD_DIR
 
 cd $BUILD_DIR
 cmake $SOURCE_DIR
 cmake --build .
+
+cd $SCRIPTS_DIR
+ln -s ../build/sudoku.cpython-36m-x86_64-linux-gnu.so .
 
 cd $SUDOKU_DIR
